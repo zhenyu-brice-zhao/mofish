556151953349d2656b8e3491b06f85f24411441f
Zhenyu Zhao <zk2020@buaa.edu.cn>
2024-01-09 08:56:20 +0800

    Initial commit

---
+++ src/main/java/com/zzy/mofish/MofishApplication.java
@@ -0,0 +1,13 @@
+package com.zzy.mofish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MofishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MofishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzy/mofish/MofishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.mofish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MofishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
